9706fca886c5b8300ce899f2f53e0e26032a5ba7
Khaw335 <kunlasatree_s@cmu.ac.th>
2021-10-20 21:16:11 +0700

    delete file

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0668346bbe4646e9867e2281b711580b'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7369fc0b-d1df-4c4d-8e1c-f22b47eec240'
-};
